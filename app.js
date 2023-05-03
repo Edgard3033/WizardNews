52b0d0e99b2dd390be2a8ae3bea8998243259b07
Edgard3033 <ed233094@outlook.com>
2023-05-03 11:56:16 -0400

    left off at Dynamic Routing

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list(); // this gets the list of post in postBank.js
+  // html to send as an output
+  const html = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>
+`;
+  //sends the message
+  res.send(html);
+});
 
 const PORT = 1337;
 
